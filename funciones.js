e22e347a4bc3c9ba32eb323f7b08d7d0777efa7e
Victor <victor18ram@outlook.es>
2019-08-29 11:07:10 -0600

    8 Funcion evitando any

--- funciones.js
+++ funciones.js
@@ -1,4 +1,15 @@
-var _this = this;
-setTimeout(function () {
-    console.log(_this);
-}, 2000);
+//setTimeout(() => {
+//    console.log(this);
+//},2000);
+function padLeft(value, padding) {
+    if (typeof padding === "number") {
+        return Array(padding + 1).join(" ") + value;
+    }
+    if (typeof padding === "string") {
+        return Array(padding.length + 1).join(" ") + value;
+    }
+    throw new Error("Expected String or number, got '" + padding + "' ");
+}
+console.log(padLeft("hello", "aaa"));
+console.log(padLeft("hello", 5));
+console.log(padLeft("hello", true));
