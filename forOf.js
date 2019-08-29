3797740631af2fd3b42282515fe5b7f09e99cc07
Victor <victor18ram@outlook.es>
2019-08-29 10:42:03 -0600

    4 For of con error por string

--- forOf.js
+++ forOf.js
@@ -3,3 +3,8 @@ for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
     var b = list_1[_i];
     console.log(b); // a, b, c
 }
+var string = "is it me you're looking for?";
+for (var _a = 0, string_1 = string; _a < string_1.length; _a++) {
+    var char = string_1[_a];
+    console.log(char);
+}
--- forOf.ts
+++ forOf.ts
@@ -2,3 +2,8 @@ let list = ["a", "b", "c"];
 for (let b of list) {
     console.log(b); // a, b, c
 }
+
+let string = "is it me you're looking for?";
+for (let char of string) {
+    console.log(char);
+}
