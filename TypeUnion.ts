b6c6f82614d2bd1ed8355c724e530a4f99fb0994
Victor <victor18ram@outlook.es>
2019-08-29 16:45:56 -0600

    19 Type Union

---
+++ TypeUnion.ts
@@ -0,0 +1,19 @@
+interface Bird {
+    fly();
+    layEggs();
+}
+
+interface Fish {
+    swim();
+    layEggs();
+}
+
+function getAnimal() : Fish | Bird {
+    var a : Fish;
+    return a;
+}
+
+let pet = getAnimal();
+
+pet.layEggs();
+pet.swim();
--- typeUnion.js
+++ typeUnion.js
@@ -2,15 +2,6 @@ function getAnimal() {
     var a;
     return a;
 }
-function isFish(pet) {
-    return pet.swim != undefined;
-}
 var pet = getAnimal();
-if (isFish(pet)) {
-    pet.swim();
-    console.log('glup');
-}
-else {
-    console.log('believe i can fly');
-    pet.fly();
-}
+pet.layEggs();
+pet.swim();
