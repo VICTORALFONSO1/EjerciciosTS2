dc5d6692bddeef6077af4a1530983284221953b8
Victor <victor18ram@outlook.es>
2019-08-30 11:48:56 -0600

    36 json async

--- promesas.js
+++ promesas.js
@@ -25,15 +25,34 @@ Promise.resolve(123)
         console.log(err.message);
     })
 */
-function iReturnPromiseAfter1Second() {
-    return new Promise(function (resolve) {
-        setTimeout(function () { return resolve("Hello world!"); }, 1000);
+/*
+function iReturnPromiseAfter1Second():Promise<string> {
+    return new Promise((resolve)=>{
+        setTimeout(()=>resolve("Hello world!"),1000);
     });
 }
+
 Promise.resolve(123)
-    .then(function (res) {
-    return iReturnPromiseAfter1Second();
+    .then((res)=>{
+        return iReturnPromiseAfter1Second();
+    })
+    .then((res)=>{
+        console.log(res);
+    });
+*/
+loadJSONAsync('good.json')
+    .then(function (val) { console.log(val); })["catch"](function (err) {
+    console.log('good.json error', err.message);
 })
-    .then(function (res) {
-    console.log(res);
+    .then(function () {
+    return loadJSONAsync('absent.json');
+})
+    .then(function (val) { console.log(val); })["catch"](function (err) {
+    console.log('absent.json error', err.message);
+})
+    .then(function () {
+    return loadJSONAsync('bad.json');
+})
+    .then(function (val) { console.log(val); })["catch"](function (err) {
+    console.log('bad.json error', err.message);
 });
--- promesas.ts
+++ promesas.ts
@@ -25,7 +25,7 @@ Promise.resolve(123)
         console.log(err.message);
     })
 */
-
+/*
 function iReturnPromiseAfter1Second():Promise<string> {
     return new Promise((resolve)=>{
         setTimeout(()=>resolve("Hello world!"),1000);
@@ -38,4 +38,27 @@ Promise.resolve(123)
     })
     .then((res)=>{
         console.log(res);
+    });
+*/
+
+loadJSONAsync('good.json')
+    .then(function (val) { console.log(val); })
+    .catch(function (err) {
+        console.log('good.json error', err.message);
+    })
+
+    .then(function () {
+        return loadJSONAsync('absent.json');
+    })
+    .then(function (val) { console.log(val); })
+    .catch(function (err) {
+        console.log('absent.json error', err.message);
+    })
+
+    .then(function () {
+        return loadJSONAsync('bad.json');
+    })
+    .then(function (val) { console.log(val); })
+    .catch(function (err) {
+        console.log('bad.json error', err.message);
     });
