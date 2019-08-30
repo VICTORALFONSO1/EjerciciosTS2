f147e88f3e63bdbb976bee9b33a9a3aed1de3606
Victor <victor18ram@outlook.es>
2019-08-30 11:04:28 -0600

    27 Desestructuraccion de arrays con estructuracion

--- desestructuracion.ts
+++ desestructuracion.ts
@@ -5,9 +5,13 @@ console.log(obj.x);
 var { x, y, z} = obj;
 console.log(x);
 */
-
+/*
 var array = [1, 2, 3];
 console.log(array[0]);
 
 var [x, y, z] = array;
 console.log(x);
+*/
+var array = [1, 2, 3, 4];
+var [x, y, ...rest] = array;
+console.log(x, y, rest);
