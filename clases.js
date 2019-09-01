5211fcb0c51130d8151a5fa822d4b89fbf7bd155
Victor <victor18ram@outlook.es>
2019-08-31 20:13:03 -0600

    48 Clase calculo

--- clases.js
+++ clases.js
@@ -1,22 +1,63 @@
-var Startup = /** @class */ (function () {
-    function Startup(texto) {
+/*
+class Startup {
+    private text: String;
+    constructor (texto: String) {
         this.text = texto;
     }
-    Startup.prototype.main = function () {
+    public main () : number {
         console.log(this.text);
         return 0;
-    };
-    return Startup;
-}());
-var s = new Startup("Hola mundo");
-s.main();
-var Startup1 = /** @class */ (function () {
-    function Startup1() {
     }
-    Startup1.main = function () {
+}
+let s = new Startup("Hola mundo");
+s.main();
+
+class Startup1 {
+    public static main(): number {
         console.log('Hola mundo');
         return 0;
-    };
-    return Startup1;
-}());
+    }
+}
 Startup1.main();
+*/
+var Calculo = /** @class */ (function () {
+    function Calculo(x, y) {
+        this.x = 0;
+        this.y = 0;
+        this.setX(x);
+        this.setY(y);
+    }
+    Calculo.prototype.setX = function (x) {
+        this.x = x;
+    };
+    Calculo.prototype.setY = function (y) {
+        this.y = y;
+    };
+    Calculo.prototype.getX = function () {
+        return this.x;
+    };
+    Calculo.prototype.getY = function () {
+        return this.y;
+    };
+    Calculo.prototype.sumar = function () {
+        return (this.getX() + this.getY());
+    };
+    Calculo.prototype.restar = function () {
+        return (this.mayor() - this.menor());
+    };
+    Calculo.prototype.menor = function () {
+        if (this.getX() >= this.getY()) {
+            return this.getY();
+        }
+        return this.getX();
+    };
+    Calculo.prototype.mayor = function () {
+        if (this.getX() >= this.getY()) {
+            return this.getX();
+        }
+        return this.getY();
+    };
+    return Calculo;
+}());
+var calculo = new Calculo(30, 10);
+console.log(calculo.restar());
