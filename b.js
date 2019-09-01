7e351ee07f8506ffd2f00ea1465533735d73153c
Victor <victor18ram@outlook.es>
2019-08-31 23:09:06 -0600

    63 Modulos

---
+++ b.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+require();
+var a = require("./a");
+var b = a;
+console.log(b);
---
+++ b.ts
@@ -0,0 +1,4 @@
+import var a = require("./a");
+var b = a;
+
+console.log(b);
